c687a7ce6ba6d512f9127070f6bc623f90db296e
Atticus <atticushornsby@gmail.com>
2013-09-26 14:54:23 -0500

    Code readability
    Added additional comments and at Kaleb's suggestion used std namespace
    instead of explicitly calling std for readability's sake.

--- changemaker/changemaker.cpp
+++ changemaker/changemaker.cpp
@@ -8,31 +8,44 @@
 #include <conio.h>
 #include <iomanip>
 
+using namespace std;
+
 int _tmain(int argc, _TCHAR* argv[])
 {
+
+//constants for extended ascii borders
 	char tl=218;			//top left corner border
 	char tr=191;			//top right corner border
 	char h=196;				//horizontal border
 	char v=179;				//vertical border
 	char lm=195;			//left middle border
 	char rm=180;			//right middle border
-	
+//variables for border width and formatting	
 	int bwidth=60;			//width of the border
 	int nbwidth=bwidth-4;	//width of nested border
 	int c1w=5;				//width of column 1
+//variables for i/o and calculations
 	double change;
-	std::cout	<< "How much change (in a dollars) needs to be given to the customer? ";
-	std::cin	>> change;
-	std::cout	<< tl << std::setw(bwidth) << std::setfill(h) << tr << std::endl							//line1 top border
-				<< v << "  Change due:" << std::setfill(' ')												//line2 output |  Change due: 
-				<< std::setw(bwidth-25) << "$ "																//line2 output $ character
-				<< std::setw(11) << std::fixed << std::setprecision(2) << change << v << std::endl			//line2 output amount of change due change with fixed point
-				<< lm << std::setw(bwidth) << std::setfill(h) << rm << std::endl							//line3 inside border
-				<< v << std::setw(bwidth) << std::setfill(' ') << v << std::endl							//line4 inside whitespace
-				<< v << " " << tl << std::setw(nbwidth) << std::setfill(h) << tr << " " << v << std::endl	//line5 inside nested border65
-				<< v << " " << v << " PAPER BILLS" << std::setw(bwidth-16) << std::setfill(' ') << v << " " << v << std::endl //line6 output PAPER BILLS
-				<< v << " " << lm << std::setw(nbwidth) << std::setfill(h) << rm << " " << v << std::endl	//line7 nested border
-				<< v << " " << v << std::setw(c1w) << std::setfill(' ') << v 
+
+
+//input
+	cout	<< "How much change (in a dollars) needs to be given to the customer? ";
+	cin		>> change;
+
+//calculations
+
+	
+//output
+	cout	<< tl << setw(bwidth) << setfill(h) << tr << endl												//line1 top border
+			<< v << "  Change due:" << setfill(' ')															//line2 output |  Change due: 
+			<< setw(bwidth-25) << "$ "																		//line2 output $ character
+			<< setw(11) << fixed << setprecision(2) << change << v << endl									//line2 output amount of change due change with fixed point
+			<< lm << setw(bwidth) << setfill(h) << rm << endl												//line3 inside border
+			<< v << setw(bwidth) << setfill(' ') << v << endl												//line4 inside whitespace
+			<< v << " " << tl << setw(nbwidth) << setfill(h) << tr << " " << v << endl						//line5 inside nested border65
+			<< v << " " << v << " PAPER BILLS" << setw(bwidth-16) << setfill(' ') << v << " " << v << endl	//line6 output PAPER BILLS
+			<< v << " " << lm << setw(nbwidth) << setfill(h) << rm << " " << v << endl						//line7 nested border
+			<< v << " " << v << setw(c1w) << setfill(' ') << v 
 				;
 	_getch();
 	return 0;
